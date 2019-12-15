8427003adb555cd6afd9dc8ee7036d16deea9ade
Jinkwan <jine2669@gmail.com>
2019-12-15 16:35:49 +0900

    re commit

---
+++ Baekjoon/src/question2447/Main.java
@@ -0,0 +1,23 @@
+package question2447;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	
+	static String[][] map;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		map = new String[3^n][3^n];
+		
+		
+		
+	}
+	
+	
+}
