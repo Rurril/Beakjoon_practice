8eca8f62d55a0a578c198e5f86eb71686d030d7d
Jinkwan <jine2669@gmail.com>
2020-01-05 22:56:01 +0900

    다른 사람이 한 풀이도 넣어서 포함(Main2)  풀이완료

--- Baekjoon/src/question11057/Main.java
+++ Baekjoon/src/question11057/Main.java
@@ -1,5 +1,40 @@
 package question11057;
 
-public class Main {
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[n][10];
+		int mod = 10007;
+		
+		for(int i = 0 ; i < 10; i++) { // 첫 n = 1 인 부분 모두 1로 초기화
+			map[0][i] = 1;
+		}
+		
+		for(int i = 1 ; i < n; i++) { //몇 번을 맵을 그리면서 해야되는가.
+			for(int j = 0; j < 10; j++) {
+				
+				if(j==0) {
+					map[i][j] = 1;
+					continue;
+				}
+				map[i][j] = map[i-1][j] + map[i][j-1];
+				map[i][j] = map[i][j]%mod;
+			}
+		}
+		
+		int result = 0;
+		for(int i = 0; i < 10; i++) {
+			result += map[n-1][i]%mod;
+		}
+		
+		System.out.println(result%mod);
+				
+	}
 }
