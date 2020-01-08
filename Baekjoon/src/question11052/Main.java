cbf5815b01dc9dbf984fbe18233e33718c09ca19
Jinkwan <jine2669@gmail.com>
2020-01-08 14:01:23 +0900

    추

--- Baekjoon/src/question11052/Main.java
+++ Baekjoon/src/question11052/Main.java
@@ -4,17 +4,26 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class Main {
+public class Main { // 카드구매하기
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String[] temp = br.readLine().split(" ");
-		int[] cost = new int[n];
-		for(int i = 0; i < n; i++) {
-			cost[i] = Integer.parseInt(temp[i]);
+		
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(temp[i-1]);
 		}
 		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i-j] + arr[j]);
+//				System.out.println("i is : " + i + " and : " + dp[i]);
+			}
+		}
 		
-		
+		System.out.println(dp[n]);
 	}
 }
